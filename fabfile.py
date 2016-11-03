7f1166fb7f489717168d875cae56be1e3d694397
Ben Keith <ben@benlk.com>
2016-11-03 12:20:23 -0400

    Add env vars to fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -3,19 +3,12 @@ from tools.fablib import *
 from fabric.api import task
 
 """
-Getting set up for the first time and using vim?
-Run:
-    :%s/TKTKPROJECT/project_name/g       # name for the project, used as the database name. This should match the umbrella repository name and the domain.wpengine.com name
-    :%s/TKTKENV/YOUR_SITE_ENV_VAR/g      # environment variable slug from INN's secrets repository
-"""
-
-"""
 Base configuration
 """
-env.project_name = 'TKTKPROJECT'
+env.project_name = 'gijn'
 env.hosts = ['localhost', ]
 env.sftp_deploy = True # needed for wpengine
-env.domain = 'TKTKPROJECT.dev'
+env.domain = 'gijn.dev'
 
 """
 Add HipChat info to send a message to a room when new code has been deployed.
@@ -31,10 +24,10 @@ def production():
     Work on production environment
     """
     env.settings    = 'production'
-    env.hosts       = [ os.environ[ 'TKTKENV_PRODUCTION_SFTP_HOST' ], ]   # ssh host for production.
-    env.user        = os.environ[ 'TKTKENV_PRODUCTION_SFTP_USER' ]        # ssh user for production.
-    env.password    = os.environ[ 'TKTKENV_PRODUCTION_SFTP_PASSWORD' ]    # ssh password for production.
-    env.domain      = 'TKTKPROJECT.wpengine.com'
+    env.hosts       = [ os.environ[ 'GIJN_PRODUCTION_SFTP_HOST' ], ]   # ssh host for production.
+    env.user        = os.environ[ 'GIJN_PRODUCTION_SFTP_USER' ]        # ssh user for production.
+    env.password    = os.environ[ 'GIJN_PRODUCTION_SFTP_PASSWORD' ]    # ssh password for production.
+    env.domain      = 'gijn.wpengine.com'
     env.port        = '2222'
 
 
@@ -44,10 +37,10 @@ def staging():
     Work on staging environment
     """
     env.settings    = 'staging'
-    env.hosts       = [ os.environ[ 'TKTKENV_STAGING_SFTP_HOST' ], ]   # ssh host for production.
-    env.user        = os.environ[ 'TKTKENV_STAGING_SFTP_USER' ],       # ssh user for production.
-    env.password    = os.environ[ 'TKTKENV_STAGING_SFTP_PASSWORD' ]    # ssh password for production.
-    env.domain      = 'TKTKPROJECT.staging.wpengine.com'
+    env.hosts       = [ os.environ[ 'GIJN_STAGING_SFTP_HOST' ], ]   # ssh host for production.
+    env.user        = os.environ[ 'GIJN_STAGING_SFTP_USER' ],       # ssh user for production.
+    env.password    = os.environ[ 'GIJN_STAGING_SFTP_PASSWORD' ]    # ssh password for production.
+    env.domain      = 'gijn.staging.wpengine.com'
     env.port        = '2222'
 
 try:
