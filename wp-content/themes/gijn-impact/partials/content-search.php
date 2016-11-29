c46aa5adf8070e4025a4618b3802e46efe0dec1e
Ben Keith <ben@benlk.com>
2016-11-29 11:26:49 -0500

    Remove URL from search results

---
+++ wp-content/themes/gijn-impact/partials/content-search.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * The template for displaying the search partial.
+ *
+ * @package Largo
+ */
+$values = get_post_custom( $post->ID );
+$entry_classes = 'entry-content';
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
+
+	<div class="<?php echo $entry_classes; ?>">
+
+		<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => __( 'Permalink to', 'largo' ) . ' ' ) )?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
+
+		<?php largo_excerpt( $post, 1, null, null, true, false ); ?>
+
+		<small class="date-link">
+			<span class="date"><?php largo_time(); ?></span>
+			<span class="sep">|</span>
+		</small>
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
