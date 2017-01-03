09c6e892b749c0959ae78674a155a72bcb4d8d10
rclations <contact@rclations.com>
2017-01-03 11:17:40 -0500

    enqueue style.css

--- wp-content/themes/gijc2015/functions.php
+++ wp-content/themes/gijc2015/functions.php
@@ -2,6 +2,11 @@
 // hides the INN logo and feed of member stories
 define( 'INN_MEMBER', FALSE );
 
+function gijn_enqueue() {
+	wp_enqueue_style( 'gijn-stylesheet', get_stylesheet_directory_uri().'/style.css' );	//often overridden by custom-less-variables version
+}
+add_action( 'wp_enqueue_scripts', 'gijn_enqueue' );
+
 // setup the custom follow widget
 require_once( get_stylesheet_directory() . '/largo-follow-gijn.php' );
 function gijn_widgets() {
