aff6eb3f3e2ce39e37a04d6d2d6271db7b1ccf78
Ben Keith <ben@benlk.com>
2017-05-30 14:14:09 -0400

    Add Creative COmmons logo to footer next to INN logo, add styles

--- wp-content/themes/gijn/Gruntfile.js
+++ wp-content/themes/gijn/Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    paths: ['less', '../largo-dev/less/inc'],
+                    paths: ['less', '../largo/less/inc'],
                     sourceMap: true,
                     outputSourceFiles: true,
                     sourceMapBasepath: path,
